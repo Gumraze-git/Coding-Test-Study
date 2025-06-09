8480a4343c376218f29eba3f9dc30c1fbada26c8
Gumraze <154658933+Gumraze-git@users.noreply.github.com>
2025-06-09 09:17:05 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 248 ms, Memory: 18060 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        for (int i = 1; i < a + 1; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
