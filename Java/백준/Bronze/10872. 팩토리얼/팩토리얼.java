42ef0372ad0f26e1d1d5031c9e5c922d9da677c7
Gumraze <154658933+Gumraze-git@users.noreply.github.com>
2025-06-17 08:24:17 +0900

    [Bronze III] Title: 팩토리얼, Time: 192 ms, Memory: 17704 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long result = 1;
+        int n = sc.nextInt();
+
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        System.out.print(result);
+    }
+}
