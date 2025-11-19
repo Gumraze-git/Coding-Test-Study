9dd682ff70c42237adcdefcbf15bdbb5f897c198
Gumraze <154658933+Gumraze-git@users.noreply.github.com>
2025-11-19 10:20:29 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 172 ms, Memory: 17680 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -11,30 +11,13 @@ public class Main {
         System.out.println(result ? 1 : 0);
         
     }
-    
+    // 문자열 직접 비교 방법 수행
     public boolean isPalindrome(String s) {
-        int start = 0;
-        int end = s.length() - 1;
-    
-        // 서로 중앙으로 이동해 나가다 겹치는 지점에 도달하면 종료
-        while (start < end) {
-            // 영숫자인지 판별하고 유효하지 않으면 한 칸씩 이동
-            if (!Character.isLetterOrDigit(s.charAt(start))) {
-                start++;
-            } else if (!Character.isLetterOrDigit(s.charAt(end))) {
-                end--;
-            } else {    // 유효한 문자라면 앞 글자와 뒷 글자를 모두 소문자로 비교하여 비교
-                if (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) {
-                    // 하나라도 일지차히 않는다면 팰린드롬이 아니므로 false 리턴
-                    return false;
-                }
-                // 앞쪽 문자는 한 칸 뒤로, 뒤쪽 문자는 한 칸 앞으로 이동
-                start++;
-                end--;
-                
-            }
-        }
-    // 무사히 종료 될 때만 팰린드롬이므로 true 리턴
-    return true;
+        // 정규식 변환후 소문자로 변경
+        String replacedString = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        // 문자열 뒤집기 후 String 타입으로 변경
+        String reversedString = new StringBuilder(replacedString).reverse().toString();
+        // 두 문자열이 동일한지 비교
+        return s.equals(reversedString);      
     }
 }
