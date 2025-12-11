8877d4a91e30a2f9062d387325460beb78e09d5c
Gumraze <154658933+Gumraze-git@users.noreply.github.com>
2025-12-11 13:49:42 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.03 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -1,6 +1,9 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = new StringBuilder(my_string).reverse().toString();
-        return answer;
+        StringBuilder sb = new StringBuilder();
+        for (int i = my_string.length() - 1; i >= 0; i--) {
+            sb.append(my_string.substring(i, i + 1));
+        }
+        return sb.toString();
     }
 }
