2c54cd59489cc8e8690775b17475299ba050fb4a
Gumraze <154658933+Gumraze-git@users.noreply.github.com>
2025-12-16 22:22:17 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.51 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -2,10 +2,8 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] numbers) {
-        Arrays.sort(numbers);  // 오름차순 정렬
-
-        // 가장 큰 두 수는 맨 뒤에 있음
-        int answer = numbers[numbers.length - 1] * numbers[numbers.length - 2];
-        return answer;
+        // numbers의 원소 중 2개를 곱해 만들 수 있는 최댓값을 return
+        Arrays.sort(numbers);
+        return numbers[numbers.length - 1] * numbers[numbers.length - 2];
     }
 }
