d63d54f6c5fe27f87205eab5e52e9c877b6931ed
Gumraze <154658933+Gumraze-git@users.noreply.github.com>
2026-01-23 18:00:57 +0900

    [level 2] Title: 의상, Time: 11.07 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,21 +2,30 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
+        // 각 종류별로 최대 1가지 의상만 착용 가능함.
+        // 의상이 일부 겹칠 수 있으나 추가로 착용한 경우는 다른 경우로 처리함.
+        // 하루에 최소 한 개 의상을 입음.
+        
         Map<String, Integer> map = new HashMap<>();
         
-        // 종류별 개수 세기
-        for (String[] cloth: clothes) {
-            String type = cloth[1];
+        for (String[] c : clothes) {
+            String type = c[1];
+            System.out.println(type);
+            
             map.put(type, map.getOrDefault(type, 0) + 1);
         }
         
-        // 조합 수 계산
         int answer = 1;
-        for (int count : map.values()) {
-            answer *= (count + 1);
+        for (int cnt : map.values()) {
+            System.out.println("cnt: " + cnt);
+            answer *= (cnt + 1);
+            
+            System.out.println("answer: " + answer);
         }
         
-        // 아무것도 안 입은 경우 제외
+        System.out.println(map);
+        
+        
         return answer - 1;
     }
 }
