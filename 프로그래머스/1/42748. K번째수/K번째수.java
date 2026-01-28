ebdf4ecc9a407e7253243da1ae22a5110b4a3e89
Gumraze <154658933+Gumraze-git@users.noreply.github.com>
2026-01-28 18:50:01 +0900

    [level 1] Title: K번째수, Time: 2.77 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,20 +2,17 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
-
-        for (int i = 0; i < commands.length; i++) {
-            int[] temp = Arrays.copyOfRange(
-                array,
-                commands[i][0] - 1, // 시작 인덱스 보정
-                commands[i][1]      // 끝 인덱스 (미포함)
-            );
-
-            Arrays.sort(temp); // 정렬
-
-            answer[i] = temp[commands[i][2] - 1]; // k번째 수
+        // 배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때 k번째 있는 수를 구하려함
+        
+        List<Integer> result = new ArrayList<>();
+        for (int[] com : commands) {
+            int[] tempArray = Arrays.copyOfRange(array, com[0]-1, com[1]);
+            Arrays.sort(tempArray);
+            result.add(tempArray[com[2] - 1]);
         }
-
+        int[] answer = result.stream()
+            .mapToInt(Integer::intValue)
+            .toArray();
         return answer;
     }
 }
