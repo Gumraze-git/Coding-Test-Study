7326711e50ccbfe46bb71a9a1b336ead6db3eba7
Gumraze <154658933+Gumraze-git@users.noreply.github.com>
2026-01-28 20:54:08 +0900

    [level 2] Title: 가장 큰 수, Time: 280.45 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,19 +2,25 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        // nCr로 푸는 문제가 아님
         String[] arr = new String[numbers.length];
+        
         for (int i = 0; i < numbers.length; i++) {
             arr[i] = String.valueOf(numbers[i]);
         }
         
-        Arrays.sort(arr, (a, b) -> 
-                   (b + a).compareTo(a + b));
+        // 커스텀 정렬 수행
+        Arrays.sort(arr, (a, b) -> (b + a).compareTo(a + b));
         
-        if (arr[0].equals("0")) return "0";
+        // 모두 0인 경우의 예외 처리
+        if (arr[0].equals("0")) {
+            return "0";
+        }
         
+        // 이어 붙이기
         StringBuilder sb = new StringBuilder();
-        for (String s : arr) sb.append(s);
+        for (String s : arr) {
+            sb.append(s);
+        }
         
         return sb.toString();
     }
