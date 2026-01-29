66cc8aabd93c0d79380844795407643e46544a8e
Gumraze <154658933+Gumraze-git@users.noreply.github.com>
2026-01-29 11:39:02 +0900

    [level 1] Title: 폰켓몬, Time: 4.92 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,23 +2,17 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        // 총 N마리의 폰켓몬 중 N/2마리를 가져도 된다.
-        // 최대한 다양한 종류의 폰켓몬을 가지려 한다.
-        // 종류의 수를 반환하다.
-        // HashSet문제
-        
+        // 총 N마리 중 N/2마리를 가져가도 됨.
+        // 가능한 많은 종류를 가져가려고 함.
+        // HashSet 사용 문제
+        int answer = 0;
         Set<Integer> set = new HashSet<>();
         
-        for (int i : nums) {       
-            set.add(i);    
+        for (int num : nums) {
+            set.add(num);
+            if (set.size() == nums.length / 2) break;
         }
-        // 전체 종류가 M개여도
-        int distinctCount = set.size();
-        // M/2개 밖에 못가져감.
-        int limit = nums.length / 2;
         
-        // 전체 종류가 가져간 종류보다 작은 경우 distinctCount
-        // 그렇지 않은 경우 최대 가져갈 수 있는 종류 limit
-        return Math.min(distinctCount, limit);
+        return set.size();
     }
 }
