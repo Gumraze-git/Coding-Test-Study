4591349fe76d7c6a96fbfe5e742d059b83404662
Gumraze <154658933+Gumraze-git@users.noreply.github.com>
2026-01-29 11:32:53 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 109.94 ms, Memory: 99.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,24 +2,26 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
+        // 한 명빼고 모든 선수가 마라톤을 완주했음.
         // 완주하지 못한 선수의 이름을 return
-        
         Map<String, Integer> map = new HashMap<>();
         
-        for (String name : participant) {
-            map.put(name, map.getOrDefault(name, 0) + 1);
+        // 참가자 매핑
+        for (int i = 0; i < participant.length; i++) {
+            map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
         }
         
-        for (String name : completion) {
-            map.put(name, map.get(name) - 1);
+        // 완주자 처리
+        for (int j = 0; j < completion.length; j++) {
+            map.put(completion[j], map.get(completion[j]) - 1);
         }
         
+        // 완주 못한 사람 찾기
         for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            if (entry.getValue() != 0) {
+            if (entry.getValue() == 1) {
                 return entry.getKey();
             }
         }
-        
         return "";
     }
 }
