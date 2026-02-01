79b4771e315b8e81ea7151e3f787b5bf09cbe8d9
Gumraze <154658933+Gumraze-git@users.noreply.github.com>
2026-02-01 20:16:34 +0900

    [level 1] Title: 최소직사각형, Time: 1.35 ms, Memory: 99.1 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,32 +1,20 @@
-import java.util.*;
 class Solution {
     public int solution(int[][] sizes) {
-        // 지갑의 크기를 결정함.
-        // 모든 명함을 수납할 수 있는 가장 작은 지갑을 만들었을 때의 크기를 return
+        // 지갑의 크기를 결정하려고 함.
+        // 모든 조건을 만족하는 크기를 결정해야함.
         
-        // 기존 데이터 형식 복사
-        int[][] answer = new int[sizes.length][2];
+        int maxWidth = 0;
+        int maxHeight = 0;
         
-        // 카드를 (min, max) 형태로 전환
-        for (int i = 0; i < sizes.length; i++) {
-            int min = Math.min(sizes[i][0], sizes[i][1]);
-            int max = Math.max(sizes[i][0], sizes[i][1]);
+        for (int[] size : sizes) {
+            int width = size[1] >= size[0] ? size[1] : size[0];
+            int height = size[1] < size[0] ? size[1] : size[0];
             
-            answer[i][0] = min;
-            answer[i][1] = max;
-
-            System.out.println(Arrays.toString(answer[i]));
+            
+            maxWidth = (maxWidth < width) ? maxWidth = width : maxWidth;
+            maxHeight = (maxHeight < height) ? maxHeight = height : maxHeight;
         }
         
-        // 지갑의 크기 결정
-        int wid = answer[0][0];
-        int hei = answer[0][1];
-        
-        for (int i = 1; i < answer.length; i++) {
-            wid = Math.max(wid, answer[i][0]);
-            hei = Math.max(hei, answer[i][1]);
-        }
-        
-        return wid * hei;
+        return maxWidth * maxHeight;
     }
 }
