8138c4fb8de7a7f6a8809012439d47e3bad36e08
Gumraze <154658933+Gumraze-git@users.noreply.github.com>
2026-02-02 12:49:59 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,24 +1,18 @@
 class Solution {
+
+    private int count;
+
     public int solution(int num) {
-        long n = num;
-        int count = 0;
-        
-        
-        while (n != 1) {
-            // 500번 시도해도 안되면 중단 
-            if (count == 500) {
-                return -1;
-            }
-            
-            // 입력된 수가 짝수이면 2로 나눔
-            if (n % 2 == 0) {
-                n /= 2;
-            } else {
-                // 홀수이면 3을 곱하고 1을 더함
-                n = n * 3 + 1;
-            }
-            count++;
-        }
-        return count;
+        count = 0;
+        return collatz((long) num);
+    }
+
+    private int collatz(long n) {
+        if (n == 1) return count;
+        if (count >= 500) return -1;
+
+        count++;
+        if (n % 2 == 0) return collatz(n / 2);
+        else return collatz(n * 3 + 1);
     }
 }
