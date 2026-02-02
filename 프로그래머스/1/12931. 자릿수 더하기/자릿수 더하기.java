bbf028c98299272d8af27bd10f9359a6f93e74fb
Gumraze <154658933+Gumraze-git@users.noreply.github.com>
2026-02-02 12:40:01 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -1,16 +1,27 @@
-// 재귀로 풀이
+// 재귀 한 번 더 연습
+import java.util.*;
+
 public class Solution {
     public int solution(int n) {
-        return digitSum(n);
+        // 자연수에 대한 각 자릿수의 합을 구해서 return하여라
+        // 각 자릿수의 합 -> n % 10으로 첫 번째 자릿 수 계산
+        // 현재 상태는 n / 10으로 업데이트
+        int answer = digitSum(n);
+
+        return answer;
     }
-    
     // 상태 정의
-    // digitSum(n) = (n % 10) + digitSum(n / 10)
+    // digitSum(n): 자연수 n의 각 자릿수의 합을 반환함.
     private int digitSum(int n) {
-        if (n < 10) {
-            return n;
+        // 종료 조건
+        if (n == 0) {
+            return 0;
         }
-        return (n % 10) + digitSum(n / 10);
+        
+        // 다음 상태
+        int sum = n % 10;
+        int next = n / 10;
+        
+        return sum + digitSum(next);
     }
-    
 }
