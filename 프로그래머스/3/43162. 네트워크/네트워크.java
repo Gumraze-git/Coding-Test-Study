dd9fc7b4efa4254a1037a101ac0662d7147844f6
Gumraze <154658933+Gumraze-git@users.noreply.github.com>
2026-02-03 10:45:20 +0900

    [level 3] Title: 네트워크, Time: 0.17 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,42 +1,43 @@
-class Solution {    
+// 20260203 연습
+class Solution {
     public int solution(int n, int[][] computers) {
-        // 컴퓨터의 개수: n
-        // 연결에 대한 정보: computers
-        // i번 컴퓨터와 j번 컴퓨터가 연결되어 있으면 computers[i][j] = 1
-        // 네트워크의 개수를 return하라
+        // 네트워크 찾기
+        // 네트워크를 찾으면 해당 네트워크와 연결된 곳들은 모두 방문 처리
         
-        // 네트워크의 수
-        int networkCount = 0;
-        // 방문한 네트워크 처리
+        // 네트워크의 개수
+        int count = 0;
+        
+        // 방문확인 배열 = 노드의 수많큼
         boolean[] visited = new boolean[n];
         
+        // 모든 노드에 대해서
         for (int i = 0; i < n; i++) {
-            // 방문하지 않은 컴퓨터
+            // 방문하지 않은 노드가 있다면
             if (!visited[i]) {
-                // 방문하지 않은 컴퓨터가 있는 네트워크 찾음
-                networkCount++;
-                
-                // 같은 네트워크에 있는 컴퓨터들 모두 방문 처리
-                dfs(i, computers, visited);
+                // 네트워크의 수 증가
+                count++;
+                // 탐색 수행
+                dfs(i, n, visited, computers);
             }
         }
         
-        return networkCount;
+        return count;
     }
-    // i번 컴퓨터와 같은 네트워크의 컴퓨터들을 모두 visited함.
-    private void dfs(int i, int[][] computers, boolean[] visited) {
-        // 나 자신 방문 처리
+    
+    // dfs 구현
+    private void dfs(int i, int n, boolean[] visited, int[][] computers) {
+        // 탐색을 시작하며 i번째는 방문 완료 처리
         visited[i] = true;
         
-        // 나와 연결된 모든 컴퓨터 확인
-        for (int j = 0; j < computers.length; j++) {
-            // 연결되어 있고, 아직 방문하지 않았다면
+        // i번 컴퓨터와 j번 컴퓨터가 연결되어 있으면 computers[i][j] = 1
+        // 모든 j에 대해서
+        for (int j = 0; j < n; j++) {
+            // 연결되어 있다는 것은 하나의 네트워크라는 의미 -> computers[i][j] == 1
+            // 연결되어 있는데 방문처리를 하지 않았다면? -> !visited[j]
             if (computers[i][j] == 1 && !visited[j]) {
-                // 해당 컴퓨터에서도 동일하게 탐색
-                dfs(j, computers, visited);
+                // 다음 방문 처리를 위한 탐색 수행
+                dfs(j, n, visited, computers);
             }
         }
-        
-        
     }
 }
