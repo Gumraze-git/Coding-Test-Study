128240b87eaf875fa64683714979086f2c796049
Gumraze <154658933+Gumraze-git@users.noreply.github.com>
2026-02-03 11:04:14 +0900

    [level 2] Title: 타겟 넘버, Time: 5.38 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,30 +1,21 @@
+// 20260203 연습
 class Solution {
-    // count 선언
-    private int target;
-    private int[] numbers;
-    
     public int solution(int[] numbers, int target) {
-        // n개의 음이 아닌 정수가 있음.
-        // 정수들의 순서를 바꾸지 않고 target으로 만들 수 있는 방법의 수를 return
-        return dfs(0, 0, numbers, target);
+        // 숫자를 더하거나 빼서 target과 동일하게 하는 방법의 개수를 return하라        
+        return dfs(0, numbers, 0, target);
     }
     
-    // 상태 정의
-    // dfs(index, sum)
-    // index까지 처리했고
-    // 현재 누적합이 sum일 때
-    // target을 만들 수 있는 경우의 수
-    private int dfs(int index, int sum, int[] numbers, int target) {
-        // 종료 조건
+    // dfs 정의: 각 단계에서 어떤 것을 수행해야하는지
+    private int dfs(int index, int[] numbers, int sum, int target) {
+        // 종료 조건: 끝까지 계산을 수행했을 때
         if (index == numbers.length) {
+            // 숫자를 다 사용했고, 이 경로가 정답이면 1
             return (sum == target) ? 1 : 0;
         }
-        // 재귀 분기
-        int plus = dfs(index + 1, sum + numbers[index], numbers, target);
-        int minus = dfs(index + 1, sum - numbers[index], numbers, target);
         
-        return plus + minus;
+        // 현재 단계에서 해야할 것
+        // 덧셈 및 뺄샘 수행
+        return dfs(index + 1, numbers, sum + numbers[index], target) + dfs(index + 1, numbers, sum - numbers[index], target);
+
     }
-    
-    
 }
