26009fc446b12ed41b852abc477d4bec00389b48
Gumraze <154658933+Gumraze-git@users.noreply.github.com>
2026-02-04 09:54:27 +0900

    [level 2] Title: 타겟 넘버, Time: 6.99 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,21 +1,20 @@
-// 20260203 연습
+// 20260204 dfs 연습
 class Solution {
     public int solution(int[] numbers, int target) {
-        // 숫자를 더하거나 빼서 target과 동일하게 하는 방법의 개수를 return하라        
-        return dfs(0, numbers, 0, target);
+        return dfs(0, 0, numbers, target);
     }
     
-    // dfs 정의: 각 단계에서 어떤 것을 수행해야하는지
-    private int dfs(int index, int[] numbers, int sum, int target) {
-        // 종료 조건: 끝까지 계산을 수행했을 때
+    // dfs: 현 상태에서 시작해서, 가능한 모든 선택을 끝까지 수행
+    // sum에서 시작해 target을 만들 수 있는 모든 경우의 수
+    private int dfs(int index, int sum, int[] numbers, int target) {
+        // 종료 조건
+        // 모든 값을 사용했을 때
         if (index == numbers.length) {
-            // 숫자를 다 사용했고, 이 경로가 정답이면 1
+            // sum이 target과 일치하면
             return (sum == target) ? 1 : 0;
         }
         
-        // 현재 단계에서 해야할 것
-        // 덧셈 및 뺄샘 수행
-        return dfs(index + 1, numbers, sum + numbers[index], target) + dfs(index + 1, numbers, sum - numbers[index], target);
+        return dfs(index + 1, sum + numbers[index], numbers, target) + dfs(index + 1, sum - numbers[index], numbers, target);
 
     }
 }
